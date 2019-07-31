5d690d609b5a42968cb4b3f56399687df10514fc
0610ot <ot1107@icloud.com>
2019-07-31 16:11:58 +0900

    トップページslick

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".carousel-top").slick({
+    prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+    nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+    autoplay:true, 
+    dots:true,
+  });
+});
